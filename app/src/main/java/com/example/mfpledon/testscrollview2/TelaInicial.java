59afc0e03c306b4dd6ab968fb13b7f3ee75c274b
Ilan Garcia <ilanelvaron24@gmail.com>
2024-03-17 17:28:29 -0300

    android studio

---
+++ app/src/main/java/com/example/mfpledon/testscrollview2/TelaInicial.java
@@ -0,0 +1,15 @@
+package com.example.mfpledon.testscrollview2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class TelaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
